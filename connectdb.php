dccd3576a71f0cb9d03526394356d80aa82dd708
yafetandromeda <33478300+yafetandromeda@users.noreply.github.com>
2018-01-15 11:31:14 +0700

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -2,7 +2,7 @@
 	$server="localhost";
 	$database="backstreetacademy";
 	$username="root";
-	$password="rahasialah";
+	$password="";
 
 	mysql_connect($server,$username,$password)
 		or die ("GAGAL TERHUBUNG");
